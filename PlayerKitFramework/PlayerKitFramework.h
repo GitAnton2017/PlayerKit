d7b5f6ad807e033b6ca98c5ece1175b395ea9b04
Anton <antonk1973@icloud.com>
2022-12-15 12:44:11 +0300

    Initial Commit

---
+++ PlayerKitFramework/PlayerKitFramework.h
@@ -0,0 +1,18 @@
+//
+//  PlayerKitFramework.h
+//  PlayerKitFramework
+//
+//  Created by Anton2016 on 15.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PlayerKitFramework.
+FOUNDATION_EXPORT double PlayerKitFrameworkVersionNumber;
+
+//! Project version string for PlayerKitFramework.
+FOUNDATION_EXPORT const unsigned char PlayerKitFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PlayerKitFramework/PublicHeader.h>
+
+
